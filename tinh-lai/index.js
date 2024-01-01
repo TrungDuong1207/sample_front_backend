3a08bfaa4fe4c907e7f74fe01e10665f337b5bed
SBC - Trung DD <trungdt.cmaxx@gmail.com>
2024-01-01 20:25:38 +0700

    tinh lai

---
+++ tinh-lai/index.js
@@ -0,0 +1,15 @@
+/**
+ Vốn cuối cùng = Vốn ban đầu x (1 + Tỷ lệ lãi ngày)^số_ngày 
+ */
+function tinhVonCuoiCung(vonBanDau, tyLeLaiNgay, soNgay) {
+    var vonCuoiCung = vonBanDau * Math.pow(1 + tyLeLaiNgay, soNgay);
+    return vonCuoiCung.toFixed(2); // Làm tròn đến 2 chữ số thập phân
+}
+
+// Ví dụ sử dụng
+var vonBanDau = 1000;
+var tyLeLaiNgay = 0.05; // 5%
+var soNgay = 30; // 1 năm
+
+var vonCuoiCung = tinhVonCuoiCung(vonBanDau, tyLeLaiNgay, soNgay);
+console.log('Vốn cuối cùng sau 1 năm là: $' + vonCuoiCung);
